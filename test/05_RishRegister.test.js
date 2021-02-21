e5e19754f5321e5f9cbfcd58301ef16e2fbfc665
riskshield <riskshield@outlook.com>
2021-02-21 15:14:53 +0800

    add product test

--- test/05_RishRegister.test.js
+++ test/05_RishRegister.test.js
@@ -2,21 +2,21 @@ const RishRegister = artifacts.require("RishRegister");
 
 contract('RishRegister', (accounts) => {
   it('test contracts names', async () => {
-    const ins3Regsiter = await RishRegister.deployed();
-    const names=await ins3Regsiter.contractNames();
+    const rishRegsiter = await RishRegister.deployed();
+    const names=await rishRegsiter.contractNames();
     assert(names.length>0,"Regsiter should have some contract on deploy");
   });
 
   it('register',async ()=>{
-    const ins3Regsiter = await RishRegister.deployed();
-    const names=await ins3Regsiter.contractNames();
+    const rishRegsiter = await RishRegister.deployed();
+    const names=await rishRegsiter.contractNames();
     const namesLength=names.length;
     const selfTag = web3.utils.toHex("SELF");
-    await ins3Regsiter.registerContract(selfTag,ins3Regsiter.address);
-    const newNamesLength=(await ins3Regsiter.contractNames()).length;
+    await rishRegsiter.registerContract(selfTag,rishRegsiter.address);
+    const newNamesLength=(await rishRegsiter.contractNames()).length;
     assert.equal(newNamesLength,namesLength+1,"Register a contract should add an name to register");
 
-    const addr=await ins3Regsiter.getContract(selfTag);
-    assert.equal(ins3Regsiter.address,addr,"The contract is not the registered one");
+    const addr=await rishRegsiter.getContract(selfTag);
+    assert.equal(rishRegsiter.address,addr,"The contract is not the registered one");
   });
 });
